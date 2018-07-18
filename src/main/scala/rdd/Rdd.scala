8753886c8c11336940957b6a15b3e2e33509b81c
charliepapa2@gmail.com <charliepapa2@gmail.com>
2018-07-18 12:09:26 +0100

    asdf

--- src/main/scala/rdd/Rdd.scala
+++ src/main/scala/rdd/Rdd.scala
@@ -1,6 +1,9 @@
 package rdd
 
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
+
+// https://spark.apache.org/docs/2.3.1/
 
 object Rdd {
   def main(args:Array[String]): Unit ={
@@ -22,13 +25,26 @@ object Rdd {
     // spark keeps track of transformations with a lineage graph to see all the RDD's daddies
 
     // actions return values, and make spark do the work
-    // example of an action:
+    // examples of an actions:
+    println("first row: "+ iris.first())
+    println("here's 3 rows: ")
+    println(iris.take(3).foreach(println))
     println("unfiltered input had " + iris.count() + " lines")
     println("filtered input had " + irisFiltered.count() + " lines")
 
     // this action would load the entire RDD in memory. Works if you have enough memory
-    iris.collect();
+    iris.collect()
 
+    // RDD's are recomputed every time you run an action on them but you can reuse them by persisting them in memory
+    iris.persist()
+    iris.first()
+    iris.count()
+
+    // use collect() to get the entire dataset: it must fit in memory
+    iris.collect().foreach(println)
+
+    // workflow: create rdd, transform, persist, query, and save
+    // stop
     sc.stop()
   }
 }
@@ -37,7 +53,7 @@ class SearchFunctions(val query: String) {
   def isMatch(s: String): Boolean = {
     s.contains(query)
   }
-  def getMatchesFunctionReference(rdd: RDD[String]): RDD[String] ={
+  /*def getMatchesFunctionReference(rdd: RDD[String]): RDD[String] = {
     rdd.map(isMatch)
-  }
+  }*/
 }
