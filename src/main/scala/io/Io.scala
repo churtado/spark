49252ce783b641648020c43b8d350463c1d64f7b
charliepapa2@gmail.com <charliepapa2@gmail.com>
2018-07-26 17:36:30 +0100

    asdfadsfasdfasdf

--- src/main/scala/io/Io.scala
+++ src/main/scala/io/Io.scala
@@ -5,6 +5,9 @@ import org.apache.log4j.Level
 import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 import org.apache.spark.{HashPartitioner}
 
+import java.io.StringReader
+import au.com.bytecode.opencsv.CSVReader
+
 object Io {
   def main(args:Array[String]): Unit = {
 
@@ -26,5 +29,27 @@ object Io {
     import spark.implicits._
 
     spark.conf.set("spark.sql.crossJoin.enabled", "true") // to enable cartesian products
+
+    // reading a simple text file:
+    val textFile = sc.textFile("D:/data/shakespeare.txt")
+
+    // same method is good for reading partitioned files:
+    // 2 files here: shakespear_1.txt shakespear_2.txt, they will get UNIONED
+    val mergedTextFiles = sc.textFile("D:/data/shakespeare/")
+
+    // if files small you get back a pairRDD with names:
+    val mergedFilesPairRDD = sc.wholeTextFiles("D:/data/shakespeare/")
+
+    println(mergedFilesPairRDD.collect().mkString(" "))
+
+    // reading csv file
+    var csv = sc.textFile("D:/data/csv_data.csv")
+    val csvResult = csv.map{ line =>
+      val reader  = new CSVReader(new StringReader(line));
+      reader.readNext();
+      //line
+    }
+
+    println(csvResult.collect().foreach(println))
   }
 }
